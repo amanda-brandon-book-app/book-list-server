1a875273b7fd7abe142ad91b120dafb123f10543
brandon-fenty <brandonfenty@gmail.com>
2018-06-20 12:00:30 -0700

    lab 1 complete

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT;
 // postgres://localhost:5432/books_app - Mac Env link
 const client = new pg.Client(process.env.DATABASE_URL);
 
-
 client.connect();
 client.on('error', err => console.log(err));
 
@@ -25,6 +24,17 @@ app.use(cors());
 app.get('/api/v1/books', (req, res) => {
     console.log('New GET request');
     let SQL = `SELECT * FROM books;`;
+    client.query(SQL)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+// Book GET w/o description
+app.get('/api/v1/books-slim', (req, res) => {
+    console.log('New slimmed GET request')
+    let SQL = `
+        SELECT book_id, title, author, image_url FROM books;
+        `;
     client.query(SQL)
     .then(results => res.send(results.rows))
     .catch(console.error);
